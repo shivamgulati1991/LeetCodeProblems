06d4e9cdfbee2382b1ac0feba3611e369db6f1ee
Shivam Gulati <shivamgulati1991@gmail.com>
2017-01-26 17:27:57 -0500

    Create 9.Palindrome.java

---
+++ 9.Palindrome.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int sum=0;
+        int num=x;
+        while(num>0){
+            sum=(sum*10) + (num%10);
+            num=num/10;
+        }
+        return (x==sum);
+    }
+}
